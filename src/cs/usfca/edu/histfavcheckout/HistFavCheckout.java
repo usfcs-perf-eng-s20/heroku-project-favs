31b1792a25d44e5d7d2bf4735b8e5d1dfc78b22f
kedarkhetia <kedarkhetia@gmail.com>
2020-02-10 02:00:20 -0800

    Hello, World test

---
+++ src/cs/usfca/edu/histfavcheckout/HistFavCheckout.java
@@ -0,0 +1,11 @@
+package cs.usfca.edu.histfavcheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistFavCheckout {
+	public static void main(String[] args) {
+		SpringApplication.run(HistFavCheckout.class, args);
+	}
+}
