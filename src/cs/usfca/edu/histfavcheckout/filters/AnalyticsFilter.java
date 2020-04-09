25181cefd3024b59973d4db713c962f3ca1684aa
Kedar Khetia <kedarkhetia@users.noreply.github.com>
2020-04-08 17:22:56 -0700

    Update AnalyticsFilter.java

--- src/cs/usfca/edu/histfavcheckout/filters/AnalyticsFilter.java
+++ src/cs/usfca/edu/histfavcheckout/filters/AnalyticsFilter.java
@@ -22,7 +22,7 @@ import cs.usfca.edu.histfavcheckout.utils.LoggerHelper;
 @Order(1)
 public class AnalyticsFilter implements Filter {
 	private static final String SERVICE_NAME = "HIST-FAV-CHECKOUT";
-	private static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+	private static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
