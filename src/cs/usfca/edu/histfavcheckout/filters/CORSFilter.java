2adffe227f444f33eb43bfb953a5b3525e0497e2
mosopeogundipe <sopeogund@gmail.com>
2020-05-04 11:20:11 -0700

    adjusting CORS response

--- src/cs/usfca/edu/histfavcheckout/filters/CORSFilter.java
+++ src/cs/usfca/edu/histfavcheckout/filters/CORSFilter.java
@@ -22,8 +22,9 @@ public class CORSFilter implements Filter {
 	    HttpServletResponse response = (HttpServletResponse) res;
 
 	    response.setHeader("Access-Control-Allow-Origin", "*");
-	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	    // response.setHeader("Access-Control-Allow-Credentials", "true");
+	    // response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Allow-Methods", "*");
 	    response.setHeader("Access-Control-Max-Age", "3600");
 	    response.setHeader("Access-Control-Allow-Headers", "*");
 
